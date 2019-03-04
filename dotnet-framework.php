2932c8ddea45fbf36a49ea3363c21d71f924127a
Oscar Gomez Elastic-Aluminum-Levitated-API <42280574+oscarg933@users.noreply.github.com>
2019-03-04 12:08:56 -0700

    Create dotnet-framework.php

---
+++ dotnet-framework.php
@@ -0,0 +1,99 @@
+716 repository results
+elevateweb/elevatezoom
+Updated on Jul 19, 2018
+
+ JavaScript
+ 976
+thomaschampagne/elevate
+Browser extension for strava. "Elevate" tracks your fitness and progressions over time. Analyse deeper your activitie…
+chrome
+opera
+typescript
+strava
+angular
+git-flow
+webextension
+running
+swim
+cycling
+MIT license
+
+Updated 2 hours ago
+
+2 issues need help
+ TypeScript
+ 542
+rsmudge/ElevateKit
+The Elevate Kit demonstrates how to use third-party privilege escalation attacks with Cobalt Strike's Beacon payload.
+Updated on Dec 9, 2017
+
+ PowerShell
+ 152
+Nike-Inc/Elevate
+Elevate is a JSON parsing framework that leverages Swift to make parsing simple, reliable and composable.
+MIT license
+
+Updated on Sep 17, 2018
+
+ Swift
+ 626
+vmcall/ElevateMe
+Handle access elevation via direct kernel object manipulation
+GPL-3.0 license
+
+Updated on Jan 26, 2018
+
+ C#
+ 99
+rootm0s/WinPwnage
+computer Elevate, UAC bypass, privilege escalation, dll hijack techniques
+persistence
+dll-hijack
+elevate
+privilege-escalation
+uac-bypass
+Updated 5 days ago
+
+ Python
+ 1.4k
+0xbadjuju/Tokenvator
+A tool to elevate privilege with Windows Tokens
+Updated 6 days ago
+
+ C#
+ 475
+igorlino/elevatezoom-plus
+Enhanced elevateZoom - A jQuery image zoom plugin
+MIT license
+
+Updated on Dec 28, 2018
+
+12 issues need help
+ JavaScript
+ 104
+jpassing/elevate
+elevate -- start elevated processes from the command line
+MIT license
+
+Updated on May 3, 2018
+
+ C
+ 74
+v5est0r/Python_FuckMySQL
+Using To MySQL Elevate Privileges.
+Updated on Aug 14, 2017
+
+ Python
+ 98
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
